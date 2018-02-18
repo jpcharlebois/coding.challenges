084d6f08c37861675ae98e2eff3b353a9d024a06
John-Paul Charlebois <charlebois.jp@gmail.com>
2018-02-18 12:46:11 -0800

    Adding 2 new coding challenges
    HammingDistance
    -and-
    LargestKStum

---
+++ HammingDistance.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+class Solution {
+  public static void main(String[] args) {
---
+++ LargestKSum.java
@@ -0,0 +1,5 @@
+/**
+ * given an array of int's who's values may range from 0-9
+ * and an int of K value find the largest sum of int's in
+ * the array for K elements.
+ */

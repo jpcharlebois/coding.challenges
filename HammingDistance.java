bd68fcd5a3a14fc9ddefb06af44b46851d692256
John-Paul Charlebois <charlebois.jp@gmail.com>
2018-02-18 12:51:28 -0800

    comment updates

--- HammingDistance.java
+++ HammingDistance.java
@@ -10,3 +10,29 @@ import java.util.*;
 
 class Solution {
   public static void main(String[] args) {
+    int x = 0;
+    int y = 0;
+    
+    String test1 = hammingDistance(x, y);
+    System.out.println(test1);
+  }
+  
+  public int hammingDistance(int x, int y) {
+    int error = 0;
+    // first convert x & y int's to binary string
+    String binaryX = Integer.toBinaryString(x);
+    String binaryY = Integer.toBinaryString(y);
+    // convert binary string's to char[]
+    char[] xBits = binaryX.toCharArray();
+    char[] yBits = binaryY.toCharArray();
+    // back fill smallest array with 0's
+    int length
+    if 
+    // loop through char[]
+    for (int i = 0; i <
+      // compare xChar[i] to yChar[i]
+          // if not same increment error count.
+          
+      // return error count;
+  }
+}
